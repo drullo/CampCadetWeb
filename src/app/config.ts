10c02d7330729ce7892df7216f0c9153b3a18d1a
Dan Rullo <drullo@cleavelandprice.com>
2025-02-16 23:10:12 -0500

    api url update

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,5 +1,5 @@
 export const config = {
-    api: 'http://www.westmorelandcampcadet.org',
+    api: '',
     emailJs: {
         publicKey: 'IzTjPBVBXcGc3LUEs',
         serviceId: 'campcadetgmail',
